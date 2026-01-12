dc7e0d783762636f6abbb0e8db4d46a7c1d2ee84
Prajwal <prajwal@example.com>
2026-01-12 14:41:08 +0530

    Added Landmarks DocType with Distance and Time fields

---
+++ land_management/land_management/doctype/landmarks/landmarks.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, PRAJWAL PAWAR and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Landmarks(Document):
+	pass
